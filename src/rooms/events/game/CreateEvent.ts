8abc0099046852d6467679f7d9e1ab3acdd7b38d
Gabriel Maximo <gbielmaximo@gmail.com>
2023-01-17 01:21:29 +0000

    vai começar o calc

--- src/rooms/events/game/CreateEvent.ts
+++ src/rooms/events/game/CreateEvent.ts
@@ -12,14 +12,25 @@ export class OnCreateCommand extends Command<GameRoom> {
         this.room.setState(new GameState());
 
         this.room.onMessage("escolha", (client: Client, message: string) => {
-            const player = this.room.state.players.get(client.sessionId)
+            const player = this.state.players.get(client.sessionId)
             if (!player.isPlaying) return
 
             const newChoice: ChoiceSchema = new ChoiceSchema();
-            newChoice.sessionId = client.sessionId;
             if (newChoice.DEFAULT_CHOICES.indexOf(message) == -1) return
+            
+            newChoice.sessionId = client.sessionId;
             newChoice.choice = message;
-            console.log(newChoice.choice)
+            
+            player.isPlaying = false;
+            client.send("espere");
+
+            const choicesList = this.room.state.choices
+            choicesList.push(newChoice);
+
+            if (choicesList.length == 2) {
+                //calcular pontos
+                this.room.state.calc();
+            }
         });
     }
 }
--- src/rooms/schema/GameState.ts
+++ src/rooms/schema/GameState.ts
@@ -1,11 +1,14 @@
-import { Schema, Context, type, MapSchema } from "@colyseus/schema";
+import { Schema, ArraySchema, type, MapSchema } from "@colyseus/schema";
 import { Client } from "colyseus";
 import { PlayerSchema } from "./PlayerSchema";
+import { ChoiceSchema } from "./ChoiceSchema";
 
 export class GameState extends Schema {
     @type({ map: PlayerSchema }) players = new MapSchema<PlayerSchema>();
     @type({ array: "string" }) order = new Array<string>();
-    @type({ array: "string" }) choices = new Array<string>();
+    @type({ array: "string" }) choices = new ArraySchema<ChoiceSchema>();
+    @type("boolean") inGame: boolean = false;
+
     start() {
         for (let i = 0; i < 2; i++) {
             const player = this.players.get(this.order[i]);
@@ -15,6 +18,10 @@ export class GameState extends Schema {
 
             player.client.send("comecar");
         }
+        this.inGame = true;
+    }
+
+    calc() {
         
     }
 }
