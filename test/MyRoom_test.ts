a0c556d63297cfd68c5e91d39ad6123bf914f09c
Gabriel Maximo <gbielmaximo@gmail.com>
2023-01-19 00:37:38 +0000

    iniciando os unit tests

--- test/MyRoom_test.ts
+++ test/MyRoom_test.ts
@@ -3,7 +3,7 @@ import { ColyseusTestServer, boot } from "@colyseus/testing";
 
 // import your "arena.config.ts" file here.
 import appConfig from "../src/arena.config";
-import { MyRoomState } from "../src/rooms/schema/MyRoomState";
+import { GameState } from "../src/rooms/schema/GameState";
 
 describe("testing your Colyseus app", () => {
   let colyseus: ColyseusTestServer;
@@ -15,7 +15,7 @@ describe("testing your Colyseus app", () => {
 
   it("connecting into a room", async () => {
     // `room` is the server-side Room instance reference.
-    const room = await colyseus.createRoom<MyRoomState>("my_room", {});
+    const room = await colyseus.createRoom<GameState>("game", {});
 
     // `client1` is the client-side `Room` instance reference (same as JavaScript SDK)
     const client1 = await colyseus.connectTo(room);
@@ -26,6 +26,6 @@ describe("testing your Colyseus app", () => {
     // wait for state sync
     await room.waitForNextPatch();
 
-    assert.deepStrictEqual({ mySynchronizedProperty: "Hello world" }, client1.state.toJSON());
+    assert.deepStrictEqual(client1.sessionId, room.state.order[0]);
   });
 });
